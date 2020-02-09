b01f71f201b5f8e42555e058a8db6bb5c444e01c
nikolaynn <kolya.by@mail.ru>
2020-02-09 23:49:45 +0300

    HW1: show mealTOs, implement meal dateformat

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.service.MealStorage;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -16,9 +19,11 @@ public class MealServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("meals", MealsUtil.filteredByStreams(MealStorage.getMeals(),
+                LocalTime.of(0, 0), LocalTime.of(23, 59, 59, 999), 2000));
+
+        log.debug("forward to meals");
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,16 +1,25 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: User
-  Date: 09.02.2020
-  Time: 22:14
-  To change this template use File | Settings | File Templates.
---%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="f" uri="http://javawebinar.ru/functions" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <html>
 <head>
     <title>Meals</title>
 </head>
 <body>
-    <p>meals works</p>
+
+<p>meals works</p>
+
+<table>
+    <c:forEach var="m" items="${requestScope.meals}">
+        <tr>
+            <td>
+                <p>${f:formatLocalDateTime(m.dateTime, 'dd.MM.yyyy HH:mm')} </p>
+            </td>
+            <td><c:out value="${m.description}"/></td>
+            <td><c:out value="${m.calories}"/></td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
